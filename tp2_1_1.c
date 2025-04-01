c6670412afcee94a2c944e83c645652b91f2e0f7
ivanncomit <fabrivancj@gmail.com>
2025-04-01 11:51:16 -0230

    Archivo para corregir codigo .c

---
+++ tp2_1_1.c
@@ -0,0 +1,18 @@
+// codigo a completar
+
+#define N 20
+
+
+int main(){
+
+int i;
+double vt[N];
+    for(i = 0;i<N; i++)
+        {
+        vt[i]=1+rand()%100;
+        printf("V[%d] - %.0f \n",i, vt[i]);
+        }
+
+
+return 0;
+}
