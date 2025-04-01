8a1d81551a9cb02bdf2740a7763e33b580d04852
ivanncomit <fabrivancj@gmail.com>
2025-04-01 12:59:08 -0230

    Editado para aritmetica de puntero dinamico indexado

--- tp2_3.c
+++ tp2_3.c
@@ -2,22 +2,37 @@
 #define M 7
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
+
+struct{
+    int unidad[M];
+} typedef Grupo;
 
 
 int main(){
 
     srand(time(NULL));
     int i,j;
-    int mt[N][M];
+
+    Grupo *grupo;
+    grupo = (Grupo *)malloc(N*sizeof(Grupo));
 
     for(i = 0;i<N; i++)
         {
         for(j = 0;j<M; j++)
             {
-            mt[i][j]=1+rand()%100;
-            printf("%d",mt[i][j]);
+            grupo[i].unidad[j]=1+rand()%100;
+            printf("%d",grupo[i].unidad[j]);
             }
         printf("\n");
     }
 
+    for(i=0;i<N;i++)
+    {
+        free(grupo[i].unidad);
+    }
+
+    free(grupo);
+
+return 0;
 }
